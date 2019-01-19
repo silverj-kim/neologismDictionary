1ced349ff5da1b64a126337d54005dfba5be2fbd
EunjeongKim <eunjeong6314@gmail.com>
2019-01-19 18:11:51 +0900

    mongoose downgrade

--- server/server.js
+++ server/server.js
@@ -1,61 +1,75 @@
 let mongoose = require('mongoose');
 mongoose.set('useFindAndModify', false);
 
-let express= require('express');
+let express = require('express');
 let app = express();
 
 let port = 8000;
 let url = "mongodb://localhost:27017/neologism";
 let wordsSchema = new mongoose.Schema({
-    name : { type: String, required: true, unique: true },
-    meaning : String,
-    searchCount : Number,
-    id : mongoose.Schema.Types.ObjectId
+    name: { type: String, required: true, unique: true },
+    meaning: String,
+    searchCount: Number,
+    id: mongoose.Schema.Types.ObjectId
 });
 
 let words = mongoose.model('words', wordsSchema);
 
-function setup(){
-    mongoose.connect(url, {useNewUrlParser: true});
-    
+function setup() {
+    mongoose.connect(url, { useNewUrlParser: true });
+
     // let agree = new words({ name : "동의보감", meaning : "동의한다", searchCount : 1 });
-    
-    app.listen(port, function(){
+
+    app.listen(port, function () {
         console.log('Neologism dictionary app listening on port ' + port);
     });
 
     route();
 }
 
-function route(){
-    app.all('/*', function(req, res, next) {
+function route() {
+    app.all('/*', function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "X-Requested-With");
         res.header('Access-Control-Allow-Headers', 'Content-Type');
         next();
-      });
+    });
 
-    app.get('/find', function(req, res){
+    app.get('/find', function (req, res) {
         let searchWord = req.query.searchWord;
 
         console.log('searchWord : ' + searchWord);
 
-        words.find({name : searchWord}, function(err, result){
+        words.find({ name: searchWord }, function (err, result) {
 
-            if(err)
+            let response = {
+                isFound: true,
+                word: searchWord,
+                meaning: ''
+            };
+
+            if (err)
                 return res.send(err);
-            
-            if(result.length === 0)
-                return res.send({
-                    word : searchWord,
-                    meaning : 'No found word'
-                });
 
-            return res.send({ 
-                word : searchWord,
-                meaning : result[0].meaning
-            });
+            if (result.length === 0){
+                response.isFound = false;
+                response.meaning = '신조어 사전에 아직 등록되지 않은 단어입니다.';
+                return res.send(response);
+            }
+            else{
+                words.updateOne({ name: searchWord }, { searchCount: result[0].searchCount + 1 }, function(){
+                    console.log(result);
+                    response.isFound = true;
+                    response.meaning = result[0].meaning;
+                    return res.send(response);
+                });
+            }
         });
+    });
+
+    app.post('/insert', function (req, res) {
+        console.log('body : ' + req.body);
+        return res.send('hi');
     });
 }
 
